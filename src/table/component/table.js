10e1ef15357738e1870f635e58b9ba77569e20ff
Noe <osorionoe@gmail.com>
2019-02-07 23:26:55 -0600

    Table component

---
+++ src/table/component/table.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+    root: {
+      width: '100%',
+      marginTop: theme.spacing.unit * 3,
+      overflowX: 'auto',
+    },
+    table: {
+      minWidth: 700,
+    },
+  });
+
+const r = 7 ,c = 5;   
+
+let id = 0;
+function createData(name, calories, fat, carbs, protein) {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein };
+}
+
+function createDinamic(r){
+    var rows = []
+    for(var i = 0; i < r; i++){
+        rows.push(createData("O", "-", "//", "O", "//"))
+    }
+    return rows;
+}
+
+function dinamicHeader(c){
+    var head = [];
+    for(var i = 0; i < c; i++){
+        head.push(i*2);
+    }
+    return head;
+}
+var headers = dinamicHeader(c)
+
+// const rows = [
+//     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//     createData('Eclair', 262, 16.0, 24, 6.0),
+//     createData('Cupcake', 305, 3.7, 67, 4.3),
+//     createData('Gingerbread', 356, 16.0, 49, 3.9),
+//   ];
+
+var rows = createDinamic(r);
+
+function SimpleTable(props) {
+    const { classes } = props;
+  
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              {headers.map(col => (
+                  
+                  <TableCell align="center">{col}</TableCell>
+              ))}
+                      
+            </TableRow>
+            
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+
+              <TableRow key={row.id}>
+                <TableCell component="th" scope="row" align="center">
+                  {row.name}
+                </TableCell>
+                <TableCell align="center">{row.calories}</TableCell>
+                <TableCell align="center">{row.fat}</TableCell>
+                <TableCell align="center">{row.carbs}</TableCell>
+                <TableCell align="center">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+
+
+
+//   function SimpleTable(props) {
+//     const { classes } = props;
+  
+//     return (
+//       <Paper className={classes.root}>
+//         <Table className={classes.table}>
+//           <TableHead>
+//             <TableRow>
+//               <TableCell>Dessert (100g serving)</TableCell>
+//               <TableCell align="right">Calories</TableCell>
+//               <TableCell align="right">Fat (g)</TableCell>
+//               <TableCell align="right">Carbs (g)</TableCell>
+//               <TableCell align="right">Protein (g)</TableCell>
+//             </TableRow>
+//           </TableHead>
+//           <TableBody>
+//             {rows.map(row => (
+//               <TableRow key={row.id}>
+//                 <TableCell component="th" scope="row">
+//                   {row.name}
+//                 </TableCell>
+//                 <TableCell align="right">{row.calories}</TableCell>
+//                 <TableCell align="right">{row.fat}</TableCell>
+//                 <TableCell align="right">{row.carbs}</TableCell>
+//                 <TableCell align="right">{row.protein}</TableCell>
+//               </TableRow>
+//             ))}
+//           </TableBody>
+//         </Table>
+//       </Paper>
+//     );
+//   }
+  
+  SimpleTable.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(SimpleTable);
+
+// class Table extends React.Component {
+//     render(){
+
+//     }
+// }
